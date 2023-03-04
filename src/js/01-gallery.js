9ad777a78fed2a70385a24590cd5844a593e3054
Rew <irinavakulenko2000@gmail.com>
2023-03-05 00:18:24 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const makeGalleryCard = ({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;   
+}
+const imagesList = document.querySelector('.gallery');
+console.log(imagesList);
+
+const markup = galleryItems.map((data) => makeGalleryCard(data)).join("");
+
+imagesList.insertAdjacentHTML('afterbegin', markup);
+
 
 console.log(galleryItems);
