defd2202653542e4ce02e4d7705de0e451633694
Rew <irinavakulenko2000@gmail.com>
2023-03-05 21:43:40 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const makeGalleryCard = ({ preview, original, description }) => {
-    return `<a class="gallery__link" href="${original}">
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
-  </a>`;   
+  </a>
+</div>`;   
 }
 const imagesList = document.querySelector('.gallery');
-console.log(imagesList);
 
 const markup = galleryItems.map((data) => makeGalleryCard(data)).join("");
 
 imagesList.insertAdjacentHTML('afterbegin', markup);
 
+const imageEl = document.querySelectorAll('.gallery__image')
 
-console.log(galleryItems);
+
+imagesList.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.nodeName!== 'IMG') {
+        return;
+    }
+    console.log(event.target.dataset)
+    const bannerUrl = event.target.dataset.source;
+    console.log(bannerUrl);
+    // bannerImgEl.src = bannerUrl;
+const instance = basicLightbox.create(`
+    <img src="${bannerUrl}" width="800" height="600">
+`)
+
+instance.show()
+}
+)
+
+
+
+
